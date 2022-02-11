e617239ce409597fb19c2992c8f86af62d860a31
Microfinance2010 <vogt@statistik.tu-dortmund.de>
2022-02-11 13:29:07 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='teach_utils',
+      version='0.1',
+      description='Utilities for courses',
+      url='http://github.com/Microfinance2010/teach_utils',
+      author='jv',
+      author_email='vogt@statistik.tu-dortmund.de,
+      license='MIT',
+      packages=['ankitpackage'],
+      zip_safe=False)
